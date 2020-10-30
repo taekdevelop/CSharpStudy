05295eab7e4966fa8f1da0ba9c7d5426d82ce672
Taek <taek351@gmail.com>
2020-10-31 02:25:00 +0900

    2020 10 31

---
+++ Chapter10/Chapter10/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+
+namespace Chapter10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Print("2번");
+            Two();
+            Print();
+
+            Print("5번");
+            Five();
+            Print();
+        }
+
+        static void Print(string message = "")
+        {
+            Console.WriteLine(message);
+        }
+
+        static void Two()
+        {
+            Matrix a = new Matrix(3, 2, 1, 4);
+            Matrix b = new Matrix(9, 2, 1, 7);
+
+            a = a.Multiply(ref b);
+        }
+
+        static void Five()
+        {
+            Hashtable ht = new Hashtable();
+
+            ht["Company"] = "Microsoft";
+            ht["URL"] = "www.microsoft.com";
+
+            Console.WriteLine($"회사 : {ht["Company"]}");
+            Console.WriteLine($"URL : {ht["URL"]}");
+        }
+    }
+}
---
+++ Chapter10/Chapter10/Two.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter10
+{
+    class Matrix
+    {
+        int[,] array = new int[2, 2];
+
+        public Matrix(int x0, int x1, int x2, int x3)
+        {
+            array[0, 0] = x0;
+            array[0, 1] = x1;
+            array[1, 0] = x2;
+            array[1, 1] = x3;
+        }
+
+        private int MultiplySingleLine(int startIndex, ref Matrix rhs, int rhsStartIndex)
+        {
+            int result = 0;
+
+            for (int i = 0; i < array.GetLength(startIndex); ++i)
+            {
+                result += array[startIndex, i] * rhs.array[i, rhsStartIndex];
+            }
+
+            return result;
+        }
+
+        public Matrix Multiply(ref Matrix rhs)
+        {
+            int x0 = MultiplySingleLine(0, ref rhs, 0);
+            int x1 = MultiplySingleLine(0, ref rhs, 1);
+            int x2 = MultiplySingleLine(1, ref rhs, 0);
+            int x3 = MultiplySingleLine(1, ref rhs, 1);
+
+            return new Matrix(x0, x1, x2, x3);
+        }
+    }
+}
