180773226e75d3547b52fccbd05c8a0d000b4cc8
Taek <taek351@gmail.com>
2020-11-02 12:32:22 +0900

    2020 11 02 12장

---
+++ Chapter12/Chapter12/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+
+            for (int i = 0; i < 10; ++i)
+            {
+                arr[i] = i;
+            }
+
+            for (int i = 0; i < 11; ++i)
+            {
+                try
+                {
+                    Console.WriteLine(arr[i]);
+                }
+                catch(IndexOutOfRangeException e) when (i >= arr.Length)
+                {
+                    Array.Resize<int>(ref arr, 11);
+                    Console.WriteLine(e.Message + " 11 크기로 변경함.");
+                }
+            }
+        }
+    }
+}
