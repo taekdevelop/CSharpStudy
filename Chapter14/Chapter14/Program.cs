92c5ffacf56c8e3e12d509fe02f9c55ad05da2f0
Taek <taek351@gmail.com>
2020-11-04 17:43:36 +0900

    2020 11 04

---
+++ Chapter14/Chapter14/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Chapter14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 11, 22, 33, 44, 55 };
+
+            Action<int> action = (int data) => Console.WriteLine(data * data);
+
+            foreach (var s in array)
+            {
+                action(s);
+            }
+        }
+    }
+}
